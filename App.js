9f2f07d596b51c4b57ecf38152ba1e4f6d37adad
Doug DeJong <dougdejong@gmail.com>
2018-10-18 19:12:42 -0400

    added scrolling lists

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Alert, Button, TextInput, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Alert, Button, TextInput, ScrollView, SectionList } from 'react-native';
 import { white } from 'ansi-colors';
 import { AppRegistry } from 'react-native';
 import HelloWorld from './src/components/HelloWorld';
@@ -14,12 +14,16 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
       <ScrollView>
+        <SectionList sections={[ {title: 'D', data: ['Doug','Derek','Dangerous','Dogs']}, {title: 'E', data: ['Erick','Elephant','Everywhere']}, {title: 'Artists', data: ['Salvador Dali','Rembrandt','im sure some other dudes too']},
+        ]}  renderItem= {({item}) => <Text style={styles.item}>{item}</Text>} 
+        renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>} 
+        keyExtractor={(item, index) => index} />
         <PizzaTranslator />
         <View style={styles.theButton}>
         <Button
         backgroundColor='green'
   onPress={() => {
-    Alert.alert('You tapped the button!');
+    Alert.alert('You tapped the button AGAIN!!!!!!!!!!');
   }}
   title="Press Me"
 />
@@ -45,5 +49,19 @@ const styles = StyleSheet.create({
   theButton: {
     color: 'red',
 
-  }
+  },
+  item: {
+    marginTop: 50,
+  },
+  sectionHeader: {
+    marginTop: 50,
+    paddingTop: 30,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'black',
+    color: 'white',
+  },
 });
